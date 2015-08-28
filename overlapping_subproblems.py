7d0c656e8550f976a7208761c45690284cb40d61
raviplakhotia <ravilakhotia2006@gmail.com>
2015-08-28 20:34:50 +0530

    fibonacci using dp

---
+++ overlapping_subproblems.py
@@ -0,0 +1,13 @@
+
+fib_lookup = dict()
+
+def fibonacci(num):
+
+   if num <= 1:
+      fib_lookup[num]=num
+   else:
+      if 'num' not in fib_lookup:
+         fib_lookup[num] = fibonacci(num-1) + fibonacci(num-2)
+   return fib_lookup[num]
+
+print (fibonacci(4))
